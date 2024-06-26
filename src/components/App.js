131424fa7b873a6ab5f63e0fbfb695f67424910a
VaishnaviRajput <rajputvaishnavikaransing10@gmail.com>
2024-06-26 20:30:00 +0530

    new

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,15 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React,{useState} from "react";
+import Menu from "./Menu";
+import Menudisplay from "./Menudisplay";
+import "../styles/App.css"
 
+function App(){
+    let [catagory,setCatogory] = useState("all");
+    return (
+        <div>
+            <Menu setCatogory={setCatogory}/>
+            <Menudisplay catagory={catagory}/>
+        </div>
+    )
+}
+export default App;
